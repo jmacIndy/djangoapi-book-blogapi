01faee0a6abe25fcb3420fee17cf8d9c5e853a55
Jeff McGinley <jeff.mcginley.indy@gmail.com>
2018-09-10 05:58:21 -0400

    added security

---
+++ posts/permissions.py
@@ -0,0 +1,17 @@
+# posts/permissions.py
+
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        # read/only permissions are allowed for any request
+
+        if request.method in permissions.SAFE_METHODS:
+
+            return True
+
+        # write permissions are only allowed to the author os post
+
+        return obj.author == request.user
